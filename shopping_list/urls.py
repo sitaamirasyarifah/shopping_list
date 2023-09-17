280be99e662ca48b8c946cbe9d34c3be7fad1baf
sitaamirasyarifah <sitaamira86@gmail.com>
2023-09-17 17:53:23 +0700

    Tutorial 2

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    path('admin/', admin.site.urls),
 ]
