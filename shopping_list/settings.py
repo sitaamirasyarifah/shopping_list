0802af7436b2067708950c4d45ec6a8c6e755019
sitaamirasyarifah <sitaamira86@gmail.com>
2023-11-22 08:58:46 +0700

    Tutorial 8

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -39,6 +39,9 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'main' ,
+    'authentication' ,
+    'corsheaders' ,
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware' ,
 ]
 
 ROOT_URLCONF = 'shopping_list.urls'
@@ -136,3 +140,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Tutorial 8
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
